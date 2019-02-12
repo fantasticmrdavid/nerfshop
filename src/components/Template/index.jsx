ef2cdd5f880e2038ee727745e6e98183947d8dea
David Li <fantasticmrdavid@gmail.com>
2019-02-12 22:27:03 +1100

    Add production placeholder message

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import Nav from 'components/Nav';
 import * as styles from './styles';
 
+const { NODE_ENV } = process.env;
+
 const Template = ({ children }) => {
   const {
     Container,
     Content,
   } = styles;
+
+  if (NODE_ENV === 'production') return 'Coming soon...';
+
   return (
     <Container>
       <Nav />
